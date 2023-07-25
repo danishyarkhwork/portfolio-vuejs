cf589cabdf9f898d40d316ae89cc96ffa373cb27
Khalid Danishyar <danishyarkhwork@gmail.com>
2023-07-25 23:29:10 +0430

    Project Setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,22 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <h5 class="white">{{ secondMsg }}</h5>
   </div>
 </template>
 
