7f510b794fae36d2d7d800204a8739d8546195eb
Khalid Danishyar <danishyarkhwork@gmail.com>
2023-07-26 13:11:06 +0430

    Structure Fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import FoodItems from './components/FoodItems.vue'
 </script>
 
 <template>
@@ -8,7 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <FoodItems />
       <HelloWorld msg="You did it!" />
+
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
